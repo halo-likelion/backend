734b58b69e82821a9fce0d042a8e393b627681f1
Dingadung <1229jimin@sju.ac.kr>
2023-07-13 01:08:11 +0900

    Set: 초기 세팅1

---
+++ src/main/java/likelion/halo/hamso/HamsoApplication.java
@@ -0,0 +1,13 @@
+package likelion.halo.hamso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/halo/hamso/HamsoApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.halo.hamso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
