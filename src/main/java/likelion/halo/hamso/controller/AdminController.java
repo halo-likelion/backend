cb3fc31395c3838ec8b03599a06a58b00195ee32
Dingadung <1229jimin@sju.ac.kr>
2023-08-13 15:38:06 +0900

    Feat: checkHealthPath

--- src/main/java/likelion/halo/hamso/controller/AdminController.java
+++ src/main/java/likelion/halo/hamso/controller/AdminController.java
@@ -78,5 +78,10 @@ public class AdminController {
         return new ResponseEntity<>(reservationService.updateReservationStatus(reservationStatusInfo.getReservationId(), reservationStatusInfo.getReservationStatus()), HttpStatus.OK);
     }
 
+    @GetMapping
+    public ResponseEntity<Void> checkHealthPath() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
