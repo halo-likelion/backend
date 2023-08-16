213232d24d63a7f649a6bd9e17a3340a7f65cc37
Dingadung <1229jimin@sju.ac.kr>
2023-08-16 20:16:37 +0900

    Add: cors() url2

--- src/main/java/likelion/halo/hamso/SecurityConfig.java
+++ src/main/java/likelion/halo/hamso/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig{
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://59.79.207.32", "http://52.79.207.32"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://59.79.207.32", "http://52.79.207.32", "http://59.79.207.32:9999", "http://52.79.207.32:9999"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
