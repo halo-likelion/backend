ee9e85dd20e3742bf0eb061b861ade7b18094e03
Dingadung <1229jimin@sju.ac.kr>
2023-08-17 22:21:44 +0900

    Add: origins에 https://letsnongsa.store 추가

--- src/main/java/likelion/halo/hamso/SecurityConfig.java
+++ src/main/java/likelion/halo/hamso/SecurityConfig.java
@@ -63,7 +63,9 @@ public class SecurityConfig{
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","localhost:1", "http://59.79.207.32", "http://52.79.207.32", "http://59.79.207.32:9999", "http://52.79.207.32:9999"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","localhost:1", "http://59.79.207.32", "http://52.79.207.32", "http://59.79.207.32:9999",
+                "http://52.79.207.32:9999", "https://52.79.207.32:9999",
+                "https://letsnongsa.store"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
 
