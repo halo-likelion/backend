5c978d52a24be3c7f64226f96c33bc9b73ad7d6b
Dingadung <1229jimin@sju.ac.kr>
2023-08-18 02:44:23 +0900

    Feat: 파일 다운로드 -> downloadImage in S3UploadService

--- src/main/java/likelion/halo/hamso/service/S3UploadService.java
+++ src/main/java/likelion/halo/hamso/service/S3UploadService.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,5 +29,17 @@ public class S3UploadService {
 
         amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
         return amazonS3.getUrl(bucket, originalFilename).toString();
+    }
+
+    public ResponseEntity<UrlResource> downloadImage(String originalFilename) {
+        UrlResource urlResource = new UrlResource(amazonS3.getUrl(bucket, originalFilename));
+
+        String contentDisposition = "attachment; filename=\"" +  originalFilename + "\"";
+
+        // header에 CONTENT_DISPOSITION 설정을 통해 클릭 시 다운로드 진행
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
+
     }
 }
